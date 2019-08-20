9497b0348c1772c72a777f3efc096ce2397e30ef
Arnaud de Surirey <adesurirey@gmail.com>
2019-08-20 12:15:38 +0200

    Clear rails cache after tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,5 +15,7 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def teardown
+    Rails.cache.clear
+  end
 end
