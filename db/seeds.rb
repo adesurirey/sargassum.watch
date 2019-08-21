3c32f0b69ce792a7d83b86d8b0e23ecd5379b210
Arnaud de Surirey <adesurirey@gmail.com>
2019-08-21 23:48:42 +0200

    Seed reports from local kml files placed in db/data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,52 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning DB..."
+
+Report.delete_all
+
+puts "Seeding reports..."
+
+def file_path(file_name)
+  Rails.root.join("db", "data", file_name)
+end
+
+def report_parser_results(kml) # rubocop:disable Metrics/AbcSize
+  puts " "
+  puts kml.name.to_s.underline
+  puts "#{kml.placemarks.size} valid placemarks"
+  puts "#{kml.errors.size} invalid placemarks".light_yellow
+
+  kml.errors.each do |error|
+    puts "— #{error}".yellow
+  end
+end
+
+def seed_form_file!(file_name, level)
+  file_path = file_path(file_name)
+  attributes = {
+    level:      level == :infested ? [:moderate, :critical].sample : level,
+    session_id: file_path.basename.to_s,
+  }
+  kml = Assets::KML.new(file_path, attributes)
+
+  Report.create!(kml.placemarks)
+  report_parser_results(kml)
+end
+
+start_time = Time.zone.now
+
+seed_form_file!("2019_beaches_with_sargassum.kml", :infested)
+seed_form_file!("2019_beaches_without_sargassum.kml", :low)
+
+seed_form_file!("2018_beaches_with_sargassum.kml", :infested)
+seed_form_file!("2018_beaches_without_sargassum.kml", :low)
+
+elapsed_time = (Time.zone.now - start_time).round
+
+puts " "
+puts "Done in #{elapsed_time} seconds."
+puts ""
+puts "#{Report.count} reports created in #{elapsed_time} seconds".underline
+puts "#{Report.low.count} low reports"
+puts "#{Report.moderate.count} moderate reports"
+puts "#{Report.critical.count} critical reports"
