cadec08fecdfb1abd0e07bc33061d010c8e568c4
Arnaud de Surirey <adesurirey@gmail.com>
2019-08-22 00:03:25 +0200

    Unit test Assets::KML::Placemark

--- test/lib/assets/kml_test.rb
+++ test/lib/assets/kml_test.rb
@@ -3,17 +3,14 @@
 require "test_helper"
 
 class Assets::KMLTest < ActiveSupport::TestCase
-  test "should return a hash of placemarks from a kml file" do
+  test "should return a hash of placemarks" do
     kml = Assets::KML.new(file_fixture("valid_placemarks.kml"))
 
-    assert_equal "2019 - Beaches without sargassum", kml.name
+    assert_equal "Valid placemarks", kml.name
     assert_equal 60, kml.placemarks.size
     assert_equal 0, kml.errors.size
 
-    assert kml.placemarks.all? { |placemark| placemark.class == Hash }
-    assert kml.placemarks.all? { |placemark| placemark[:created_at].class == DateTime }
-    assert kml.placemarks.all? { |placemark| placemark[:latitude].class == Float }
-    assert kml.placemarks.all? { |placemark| placemark[:longitude].class == Float }
+    assert kml.placemarks.all?
   end
 
   test "should accept custom placemark attributes" do
@@ -29,17 +26,24 @@ class Assets::KMLTest < ActiveSupport::TestCase
 
     assert_equal custom_attributes[:level], kml.placemarks.first[:level]
     assert_equal custom_attributes[:session_id], kml.placemarks.first[:session_id]
-    assert kml.placemarks.all? { |placemark| placemark[:created_at] }
-    assert kml.placemarks.all? { |placemark| placemark[:latitude] }
-    assert kml.placemarks.all? { |placemark| placemark[:longitude] }
+    assert kml.placemarks.first[:created_at].present?
+    assert kml.placemarks.first[:latitude].present?
+    assert kml.placemarks.first[:longitude].present?
   end
 
-  test "should ignore invalid placemarks" do
+  test "should ignore invalid placemarks and store errors" do
     kml = Assets::KML.new(file_fixture("invalid_placemarks.kml"))
 
-    assert_equal "2019 - Beaches without sargassum", kml.name
+    assert_equal "Invalid placemarks", kml.name
     assert_equal 1, kml.placemarks.size
     assert_equal 7, kml.errors.size
+
     assert_equal "No date found in name: Riu dunamar - Riviera Maya", kml.errors.first
+    assert_equal "No date found in name: 01/01/219 Riu dunamar - Riviera Maya", kml.errors[1]
+    assert_equal "Invalid date in name: 31/02/2019 Riu dunamar - Riviera Maya", kml.errors[2]
+    assert_equal "No name found", kml.errors[3]
+    assert_equal "No name found", kml.errors[4]
+    assert_equal "No coordinates found", kml.errors[5]
+    assert_equal "No coordinates found", kml.errors.last
   end
 end
