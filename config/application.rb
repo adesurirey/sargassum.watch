60fff9d61d693e240f958da6553fc7178c4558e4
Arnaud de Surirey <adesurirey@gmail.com>
2019-08-21 18:35:15 +0200

    Load files in lib folder

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,17 @@ Bundler.require(*Rails.groups)
 
 module SargassumWatch
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
+
     config.generators do |generate|
       generate.assets false
       generate.helper false
       generate.test_framework  :test_unit, fixture: false
     end
-
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
