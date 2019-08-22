9778f380bd5fe196acc4b9630002fd6f2aad2d8f
Arnaud de Surirey <adesurirey@gmail.com>
2019-08-22 02:05:47 +0200

    Add name to reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_140534) do
+ActiveRecord::Schema.define(version: 2019_08_21_223733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_140534) do
     t.string "session_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["latitude", "longitude"], name: "index_reports_on_latitude_and_longitude"
   end
 
