197ee93d290a8b0e839662b416475703dd5734a9
Arnaud de Surirey <adesurirey@gmail.com>
2019-08-22 02:07:03 +0200

    Extract formatted names from kml files

--- test/lib/assets/kml/placemark_test.rb
+++ test/lib/assets/kml/placemark_test.rb
@@ -8,9 +8,17 @@ class Assets::KML::PlacemarkTest < ActiveSupport::TestCase
     placemark = Assets::KML::Placemark.new(node)
 
     assert_kind_of Hash, placemark.attributes
+    assert_kind_of String, placemark.attributes[:name]
     assert_kind_of DateTime, placemark.attributes[:created_at]
     assert_kind_of Float, placemark.attributes[:latitude]
     assert_kind_of Float, placemark.attributes[:longitude]
+  end
+
+  test "should cleanup name and remove dates from it" do
+    node = Nokogiri::XML(valid_kml).css("Placemark")
+    placemark = Assets::KML::Placemark.new(node)
+
+    assert_equal "Anses d'Arlet", placemark.attributes[:name]
   end
 
   private
@@ -18,7 +26,7 @@ class Assets::KML::PlacemarkTest < ActiveSupport::TestCase
   def valid_kml
     <<~KML
       <Placemark>
-        <name>01/01/2019 Anses d'Arlet - Martinique></name>
+        <name>01/01/2019 Anses d'Arlet - Martinique </name>
         <Point>
           <coordinates>
             -61.0841083,14.4986926,0
