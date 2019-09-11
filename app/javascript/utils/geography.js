23884e7403f2a6558091f7892938dfd2d405b6f4
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-11 09:13:22 +0200

    Limit geocoder results sargassum concerned areas

---
+++ app/javascript/utils/geography.js
@@ -0,0 +1,11 @@
+const sargassumBbox = {
+  ne: { lng: -34.31067179238596, lat: 46.03500843336249 },
+  sw: { lng: -113.41223429238529, lat: -9.32997469975038 },
+};
+
+const sargassumCenter = {
+  latitude: 20.827873989993776,
+  longitude: -73.86145304236818,
+};
+
+export { sargassumBbox, sargassumCenter };
