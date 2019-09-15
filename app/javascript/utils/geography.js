38ce59cb633d58c7c5cac86be2a24499b46c30bb
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-15 23:48:20 +0200

    Wait for map idle event to check water features around user

--- app/javascript/utils/geography.js
+++ app/javascript/utils/geography.js
@@ -1,5 +1,3 @@
-import cheapRuler from 'cheap-ruler';
-
 const sargassumBbox = {
   ne: { lng: -34.31067179238596, lat: 46.03500843336249 },
   sw: { lng: -113.41223429238529, lat: -9.32997469975038 },
@@ -10,11 +8,4 @@ const sargassumCenter = {
   longitude: -73.86145304236818,
 };
 
-const bboxAround = ({ longitude, latitude }, squareMeters) => {
-  const ruler = cheapRuler(latitude, 'meters');
-
-  // Returns [w, s, e, n]
-  return ruler.bufferPoint([longitude, latitude], squareMeters / 2);
-};
-
-export { sargassumBbox, sargassumCenter, bboxAround };
+export { sargassumBbox, sargassumCenter };
---
+++ app/javascript/utils/map.js
@@ -0,0 +1,28 @@
+import cheapRuler from 'cheap-ruler';
+
+const bboxAround = ({ longitude, latitude }, squareMeters) => {
+  const ruler = cheapRuler(latitude, 'meters');
+
+  return ruler.bufferPoint([longitude, latitude], squareMeters / 2);
+  // [w, s, e, n]
+};
+
+const validateWaterPresence = (map, { latitude, longitude }) => {
+  const [w, s, e, n] = bboxAround({ longitude, latitude }, 50);
+
+  const bbox = [map.project([w, s]), map.project([e, n])];
+  const waterFeatures = map.queryRenderedFeatures(bbox, {
+    layers: ['water'],
+  });
+
+  return !!waterFeatures.length;
+};
+
+const onNextIdle = (map, call) => () => {
+  map.on('idle', function callback() {
+    call(map);
+    map.off('idle', callback);
+  });
+};
+
+export { onNextIdle, validateWaterPresence };
