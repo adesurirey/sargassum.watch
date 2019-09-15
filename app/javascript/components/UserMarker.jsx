ccd82f927431e6c8b8e485cd7c49d258e69aea62
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-15 19:38:49 +0200

    Add a marker at user's position

---
+++ app/javascript/components/UserMarker.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Marker } from 'react-map-gl';
+
+import { makeStyles } from '@material-ui/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { Lens } from '@material-ui/icons';
+
+const size = 52;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: size,
+    width: size,
+    position: 'relative',
+    padding: theme.spacing(2),
+    background: fade(theme.palette.primary.main, 0.1),
+    borderRadius: '50%',
+    lineHeight: 0,
+  },
+}));
+
+const UserMarker = props => {
+  const classes = useStyles();
+
+  return (
+    <Marker offsetLeft={-size / 2} offsetTop={-size / 2} {...props}>
+      <div className={classes.root}>
+        <Lens color="primary" fontSize="small" />
+      </div>
+    </Marker>
+  );
+};
+
+export default UserMarker;
