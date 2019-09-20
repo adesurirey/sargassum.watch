07ff9d3b02b00178c8668dca087447901b6b9a2b
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-20 20:33:34 +0200

    Redirect user to localized route

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Router, Redirect } from '@reach/router';
+import { Router, Redirect, redirectTo } from '@reach/router';
 
 import fallbackLng from '../i18n/fallbackLng';
 import withTheme from '../styles/withTheme';
@@ -9,21 +9,31 @@ import Spinner from './Spinner';
 const Map = lazy(() => import('./Map'));
 
 const App = () => {
-  const { i18n } = useTranslation();
+  const {
+    i18n: { services, languages },
+  } = useTranslation();
 
-  const locales = Object.keys(i18n.services.resourceStore.data);
+  const locales = Object.keys(services.resourceStore.data);
   const variants = locales.filter(locale => locale !== fallbackLng);
+
+  const redirectToVariant = () => {
+    const locale = languages[0];
+
+    if (locale !== fallbackLng) {
+      redirectTo(locale);
+    }
+  };
 
   return (
     <Suspense fallback={<Spinner delay={100} fullscreen />}>
       <Router>
-        <Map path="/" />
+        <Map path="/" onDidMount={redirectToVariant} />
 
         {variants.map(locale => (
           <Map key={locale} path={locale} />
         ))}
 
-        <Redirect from="*" to="/" noThrow />
+        <Redirect from="*" to="/" />
       </Router>
     </Suspense>
   );
