0400038b19613c9d532949297f4ab6e2b0c32e32
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-20 17:12:54 +0200

    Detect user locale

--- app/javascript/i18n/index.js
+++ app/javascript/i18n/index.js
@@ -1,19 +1,22 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from './en.json';
 
 const resources = { en };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-  fallbackLng: 'en',
-  debug: true,
-  keySeparator: false, // we use content as keys
-  interpolation: {
-    escapeValue: false, // no need for react
-  },
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    debug: true,
+    keySeparator: false, // we use content as keys
+    interpolation: {
+      escapeValue: false, // no need for react
+    },
+  });
 
 export default i18n;
