97a0c6839bbc3a7db94995ade8c20a4c7f131c83
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-20 20:32:25 +0200

    Configure language detection using path and navigator

--- app/javascript/i18n/index.js
+++ app/javascript/i18n/index.js
@@ -13,9 +13,32 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    detection: {
+      // order and from where user language should be detected
+      order: ['path', 'navigator'],
+
+      // keys or params to lookup language from
+      // lookupQuerystring: 'lng',
+      // lookupCookie: 'i18next',
+      // lookupLocalStorage: 'i18nextLng',
+      lookupFromPathIndex: 0,
+      // lookupFromSubdomainIndex: 0,
+      //
+      // cache user language on
+      // caches: ['localStorage', 'cookie'],
+      // excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+      //
+      // optional expire and domain for set cookie
+      // cookieMinutes: 10,
+      // cookieDomain: 'myDomain',
+      //
+      // optional htmlTag with lang attribute, the default is:
+      // htmlTag: document.documentElement
+    },
     resources,
     fallbackLng,
-    debug: true,
+    load: 'languageOnly', // only load matched language
+    debug: process.env.NODE_ENV === 'development',
     keySeparator: false, // we use content as keys
     interpolation: {
       escapeValue: false, // no need for react
