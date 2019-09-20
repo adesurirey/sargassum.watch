8b3f922cad7e3880e8f748dcafac1aca1a859c27
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-20 20:30:58 +0200

    Create routes for all available locales

--- app/javascript/i18n/index.js
+++ app/javascript/i18n/index.js
@@ -2,16 +2,19 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import en from './en.json';
+import fallbackLng from './fallbackLng';
+import en from './locales/en.json';
+import es from './locales/es.json';
+import fr from './locales/fr.json';
 
-const resources = { en };
+const resources = { en, es, fr };
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: 'en',
+    fallbackLng,
     debug: true,
     keySeparator: false, // we use content as keys
     interpolation: {
