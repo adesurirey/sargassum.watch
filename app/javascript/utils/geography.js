852dfdb834171c3e45add667c245009d56ff5ca2
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-27 16:10:14 +0200

    Hash map coordinates

--- app/javascript/utils/geography.js
+++ app/javascript/utils/geography.js
@@ -1,11 +1,30 @@
 const sargassumBbox = {
-  ne: { lng: -34.31067179238596, lat: 46.03500843336249 },
-  sw: { lng: -113.41223429238529, lat: -9.32997469975038 },
+  ne: { lng: -34.31, lat: 46.04 },
+  sw: { lng: -113.41, lat: -9.33 },
 };
 
 const sargassumCenter = {
-  latitude: 20.827873989993776,
-  longitude: -73.86145304236818,
+  latitude: 20.83,
+  longitude: -73.86,
+  zoom: 3,
 };
 
-export { sargassumBbox, sargassumCenter };
+const getViewport = hash => {
+  if (!hash) {
+    return sargassumCenter;
+  }
+
+  const [zoom, latitude, longitude] = hash.substr(1).split('/');
+
+  if (!zoom || !latitude || !longitude) {
+    return sargassumCenter;
+  }
+
+  return {
+    latitude: parseFloat(latitude),
+    longitude: parseFloat(longitude),
+    zoom: parseFloat(zoom),
+  };
+};
+
+export { sargassumBbox, sargassumCenter, getViewport };
