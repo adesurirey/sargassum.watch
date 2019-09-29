beec7a6655334f3f8f167cda95657e610c382acf
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-29 02:23:35 +0200

    Use scrapper to seed reports

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,17 @@
 # frozen_string_literal: true
 
-# WARNING:
-# Required kml files are not commited in this repository,
-# add your own files to db/data/ or ask for it to the owner of the repo.
-
-puts "Cleaning db..."
-
-Report.delete_all
-Dataset.delete_all
-
-puts "Seeding reports..."
-
-def report_parser_results(kml) # rubocop:disable Metrics/AbcSize
-  puts " "
-  puts kml.name.to_s.underline
-  puts "#{kml.placemarks.size} valid placemarks"
-  puts "#{kml.errors.size} invalid placemarks".light_yellow
-
+def puts_errors(kml)
   kml.errors.each do |error|
     puts "— #{error}".yellow
   end
+end
+
+def report_parser_results(kml)
+  puts ""
+  puts kml.name.to_s.underline
+  puts "#{kml.placemarks.size} valid placemarks"
+  puts "#{kml.errors.size} invalid placemarks".light_yellow
+  puts_errors(kml)
 end
 
 def report_attibutes(kind)
@@ -56,29 +48,37 @@ def random_seed(placemarks, kind)
   end
 end
 
-def seed_form_file!(file_name, kind)
-  file_path = Rails.root.join("db", "data", file_name)
+def seed(year, kind)
   attributes = report_attibutes(kind)
-  kml = Assets::KML.new(file_path, attributes)
+
+  file = Scrapper.call(kind: kind, year: year)
+  kml = Assets::KML.new(file, attributes)
 
   random_seed(kml.placemarks, kind)
 
   report_parser_results(kml)
 end
 
+puts "[1/3] Cleaning db..."
+
+Report.delete_all
+Dataset.delete_all
+
+puts "[2/3] Seeding reports..."
+
 start_time = Time.zone.now
 
 # Reports for 2019
-seed_form_file!("2019_beaches_with_sargassum.kml", :with)
-seed_form_file!("2019_beaches_without_sargassum.kml", :without)
+seed("2019", :with)
+seed("2019", :without)
 
 # Reports for 2018
-seed_form_file!("2018_beaches_with_sargassum.kml", :with)
-seed_form_file!("2018_beaches_without_sargassum.kml", :without)
+seed("2018", :with)
+seed("2018", :without)
 
 elapsed_time = (Time.zone.now - start_time).round
 
-puts " "
+puts ""
 puts "Done in #{elapsed_time} seconds.".light_green
 puts ""
 puts "#{Report.count} reports created".underline
@@ -87,9 +87,17 @@ puts "#{Report.moderate.count} moderate reports"
 puts "#{Report.na.count} na reports"
 puts "#{Report.critical.count} critical reports"
 
-puts " "
-puts "Starting reports monthly packing job..."
+puts ""
+puts "[3/3] Starting reports monthly packing job..."
+
+start_time = Time.zone.now
+
 PackLastMonthReportsJob.perform_now
 
-puts "#{Dataset.count} dataset created with #{Dataset.last.count} features"
+elapsed_time = (Time.zone.now - start_time).round
+
+puts ""
+puts "Done in #{elapsed_time} seconds.".light_green
+puts ""
+puts "#{Dataset.count} dataset created with #{Dataset.last.count} features".underline
 puts "#{Report.count} reports remaining"
