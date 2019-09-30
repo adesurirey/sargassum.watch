ff7ddfe29a05732d2667f466585db66862d35311
Arnaud de Surirey <adesurirey@gmail.com>
2019-09-30 17:57:48 +0200

    Update report form with icons and tooltips

--- app/javascript/components/LevelSubmit.jsx
+++ app/javascript/components/LevelSubmit.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { number, string, func, bool } from 'prop-types';
-import { useTranslation } from 'react-i18next';
 import clsx from 'clsx';
 
 import { makeStyles } from '@material-ui/styles';
-import { Typography, Grid, Fab } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+import { Grid, Tooltip, Zoom, Fab } from '@material-ui/core';
+import {
+  SentimentSatisfiedRounded,
+  SentimentDissatisfiedRounded,
+} from '@material-ui/icons';
+
+import SentimentNeutral from '../icons/SentimentNeutral';
 
 import { arrayToObject } from '../utils/root';
 
@@ -20,6 +26,7 @@ const propTypes = {
 const useStyles = makeStyles(theme => {
   const colors = arrayToObject(levels, (obj, level) => {
     obj[level.label] = {
+      boxShadow: '0 -2px 0 rgba(0,0,0,0.08) inset',
       backgroundColor: theme.palette.level[level.label].main,
       '&:hover': {
         backgroundColor: theme.palette.level[level.label].dark,
@@ -28,12 +35,21 @@ const useStyles = makeStyles(theme => {
   });
 
   return {
-    root: {
+    fab: {
       boxShadow: 'none',
+    },
+    label: {
+      color: theme.palette.common.white,
     },
     ...colors,
   };
 });
+
+const icons = {
+  clear: SentimentSatisfiedRounded,
+  moderate: SentimentNeutral,
+  critical: SentimentDissatisfiedRounded,
+};
 
 const LevelSubmit = ({ value, label, onClick, disabled }) => {
   const { t } = useTranslation();
@@ -45,25 +61,28 @@ const LevelSubmit = ({ value, label, onClick, disabled }) => {
     }
   };
 
+  const Icon = icons[label];
+
   return (
     <Grid item>
-      <Grid container direction="column" alignItems="center">
-        <Grid item>
-          <Fab
-            classes={{ root: clsx(classes.root, classes[label]) }}
-            color="inherit"
-            size="small"
-            type="submit"
-            aria-label={label}
-            onClickCapture={handleClick}
-          >
-            {' '}
-          </Fab>
-        </Grid>
-        <Grid item>
-          <Typography variant="caption">{t(label)}</Typography>
-        </Grid>
-      </Grid>
+      <Tooltip
+        title={t(`${label} detailed`)}
+        TransitionComponent={Zoom}
+        enterDelay={700}
+      >
+        <Fab
+          classes={{
+            root: clsx(classes.fab, classes[label]),
+            label: classes.label,
+          }}
+          color="inherit"
+          type="submit"
+          aria-label={label}
+          onClickCapture={handleClick}
+        >
+          <Icon fontSize="large" />
+        </Fab>
+      </Tooltip>
     </Grid>
   );
 };
