4ecd27be1e33acdf2eae70fcd8058b34c35fecdf
Arnaud de Surirey <adesurirey@gmail.com>
2019-10-05 00:25:56 +0200

    Configure active admin with basic http auth

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,15 @@ Rails.application.routes.draw do
     # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
     # - Use & (do not use &&) so that it doesn't short circuit.
     # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
-    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username.to_s), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"].to_s)) &
-      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password.to_s), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"].to_s))
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username.to_s), ::Digest::SHA256.hexdigest(ENV.fetch("ADMIN_USERNAME").to_s)) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password.to_s), ::Digest::SHA256.hexdigest(ENV.fetch("ADMIN_PASSWORD").to_s))
   end
 
   require "sidekiq/web"
   Sidekiq::Web.use middleware, &auth_proc
   mount Sidekiq::Web, at: "/sidekiq"
+
+  ActiveAdmin.routes(self)
 
   get ':language', to: 'pages#home'
 end
