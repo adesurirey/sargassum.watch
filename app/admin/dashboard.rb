e9ac0bb642681d9c66373ea39fcfe4db146f05d0
Arnaud de Surirey <adesurirey@gmail.com>
2019-10-05 01:51:42 +0200

    [admin] create a dashboard with main counts

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,31 +4,33 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "👀 Last scrappings" do
+          logs = ScrapperLog.last(2)
+
+          div.h2 do
+            logs.each do |log|
+              status_tag(l(log.updated_at, format: :short), class: log.level)
+            end
+          end
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    columns do
+      column do
+        panel "👀 Total reports" do
+          h2.h1 do
+            number_with_delimiter Report.count + Dataset.pluck(:count).reduce(0, :+)
+          end
+        end
+      end
+      column do
+        panel "👀 Uniq reporters since #{l Dataset.last.created_at, format: :long}" do
+          h2.h1 { number_with_delimiter Report.pluck(:user_id).uniq.count }
+        end
+      end
+    end
   end
 end
