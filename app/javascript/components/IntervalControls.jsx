f6c8b3347c8e9d68fef3f5b16f5e34f68f3e601b
Arnaud de Surirey <adesurirey@gmail.com>
2019-10-06 15:03:44 +0200

    Redesign interval controls with toggle button group

--- app/javascript/components/IntervalControls.jsx
+++ app/javascript/components/IntervalControls.jsx
@@ -1,44 +1,71 @@
 import React, { memo } from 'react';
 import { bool, func } from 'prop-types';
+import { useTranslation } from 'react-i18next';
 
+import { makeStyles } from '@material-ui/styles';
 import { Grid, Slide } from '@material-ui/core';
+import { ToggleButtonGroup, ToggleButton } from '@material-ui/lab';
 
 import { interval } from '../utils/propTypes';
-import { intervals } from '../utils/interval';
-import IntervalControl from './IntervalControl';
+import { intervals, toString } from '../utils/interval';
+import {
+  fullwidthClasses,
+  buttonSmallClasses,
+} from '../styles/ToggleButtonGroup';
 
 const propTypes = {
   loaded: bool,
   selectedInterval: interval,
-  onIntervalChange: func.isRequired,
+  onChange: func.isRequired,
 };
 
 const defaultProps = {
   loaded: false,
 };
 
-const IntervalControls = ({ loaded, selectedInterval, onIntervalChange }) => (
-  <Grid item xs={12}>
-    <Grid container justify="space-evenly" role="group">
-      {intervals.map((interval, index) => (
-        <Slide
-          key={interval.id}
-          in={loaded}
-          direction="right"
-          style={{ transitionDelay: index * 50 }}
-        >
-          <Grid item>
-            <IntervalControl
-              interval={interval}
-              active={selectedInterval.id === interval.id}
-              onClick={onIntervalChange}
-            />
-          </Grid>
-        </Slide>
-      ))}
+const useStyles = makeStyles(theme => ({
+  ...fullwidthClasses,
+  ...buttonSmallClasses,
+}));
+
+const IntervalControls = ({ loaded, selectedInterval, onChange }) => {
+  const { t } = useTranslation();
+  const classes = useStyles();
+
+  const handleChange = (_e, intervalId) =>
+    onChange(intervals.find(({ id }) => id === intervalId));
+
+  return (
+    <Grid item xs={12}>
+      <ToggleButtonGroup
+        aria-label={t('interval control')}
+        classes={{
+          root: classes.buttonGroup,
+          grouped: classes.button,
+          groupedSizeSmall: classes.buttonSmall,
+        }}
+        size="small"
+        value={selectedInterval.id}
+        exclusive
+        onChange={handleChange}
+      >
+        {intervals.map((interval, index) => (
+          <Slide
+            key={interval.id}
+            in={loaded}
+            direction="right"
+            style={{ transitionDelay: index * 50 }}
+            value={interval.id}
+          >
+            <ToggleButton value={interval.id}>
+              {t(toString(interval))}
+            </ToggleButton>
+          </Slide>
+        ))}
+      </ToggleButtonGroup>
     </Grid>
-  </Grid>
-);
+  );
+};
 
 export default memo(IntervalControls);
 
