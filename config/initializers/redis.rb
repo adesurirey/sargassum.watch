e555b45af5ec14c85769e61b75d3d8b515237cf1
Arnaud de Surirey <adesurirey@gmail.com>
2019-10-08 00:44:51 +0200

    Configure redis for production

---
+++ config/initializers/redis.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+$redis = Redis.new
+
+url = ENV["REDISCLOUD_URL"]
+
+if url
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: url }
+  end
+  $redis = Redis.new(url: url)
+end
