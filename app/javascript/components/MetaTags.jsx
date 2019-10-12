a7b4ba25791e49ebe41ba6a3bca61a5cccccbff6
Arnaud de Surirey <adesurirey@gmail.com>
2019-10-12 20:14:26 +0200

    Create basic meta tags

---
+++ app/javascript/components/MetaTags.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useTranslation } from 'react-i18next';
+
+import { currentLanguage } from '../utils/i18n';
+
+const MetaTags = () => {
+  const { t, i18n } = useTranslation();
+
+  const language = currentLanguage(i18n);
+  const url = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
+
+  // If params interval || hash
+  // <link rel="canonical" href="http://mysite.com/example" />
+
+  return (
+    <Helmet>
+      <title>{t('title')}</title>
+      <meta name="description" content={t('description')} />
+
+      <meta property="og:title" content={t('title')} />
+      <meta property="og:url" content={url} />
+      <meta property="og:locale" content={language} />
+      <meta property="og:description" content={t('description')} />
+
+      <meta name="twitter:title" content={t('title')} />
+      <meta name="twitter:description" content={t('description')} />
+    </Helmet>
+  );
+};
+
+export default MetaTags;
