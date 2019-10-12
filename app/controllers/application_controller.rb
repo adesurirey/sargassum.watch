449d14d963e453f5c68a3747ceba6f72bed3bfea
Arnaud de Surirey <adesurirey@gmail.com>
2019-10-12 20:13:17 +0200

    Setup Rails locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :authenticate_user
 
   private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
 
   def authenticate_user
     return if user_id
