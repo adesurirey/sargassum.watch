42562b6ca1d706732d92f884589e4b20774ce015
Arnaud de Surirey <adesurirey@gmail.com>
2019-10-26 18:11:05 +0200

    [Admin] Add webcams stats to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     columns do
       column do
-        panel "👀 Last scrappings" do
+        panel "👀 Last report scrappings" do
           logs = ScrapperLog.last(2)
 
           div.h2 do
@@ -27,9 +27,39 @@ ActiveAdmin.register_page "Dashboard" do
         end
       end
       column do
-        panel "👀 Uniq reporters since #{l Dataset.last.created_at, format: :long}" do
+        since = Dataset.any? ? l(Dataset.last.created_at, format: :long) : "the beginning"
+
+        panel "👀 Uniq reporters since #{since}" do
           h2.h1 do
             number_with_delimiter Report.original.pluck(:user_id).uniq.count
+          end
+        end
+      end
+    end
+
+    columns do
+      last_webcam = Webcam.order(created_at: :asc).last
+
+      column do
+        panel "👀 Total webcams" do
+          h2.h1 do
+            number_with_delimiter Webcam.count
+          end
+        end
+      end
+      column do
+        panel "👀 Last webcam create" do
+
+          h2.h1 do
+            l(last_webcam.created_at, format: :short)
+          end
+        end
+      end
+      column do
+        panel "👀 Last webcam update" do
+
+          h2.h1 do
+            l(last_webcam.updated_at, format: :short)
           end
         end
       end
