c0b950bfc175f019ee4822558926fa391f243a9b
Arnaud de Surirey <adesurirey@gmail.com>
2019-11-01 13:36:53 +0100

    Retry chunk loading on network error

---
+++ app/javascript/utils/retry.js
@@ -0,0 +1,20 @@
+// Source: https://dev.to/goenning/how-to-retry-when-react-lazy-fails-mb5
+
+export default function retry(fn, retriesLeft = 5, interval = 1000) {
+  return new Promise((resolve, reject) => {
+    fn()
+      .then(resolve)
+      .catch(error => {
+        setTimeout(() => {
+          if (retriesLeft === 1) {
+            // reject('maximum retries exceeded');
+            reject(error);
+            return;
+          }
+
+          // Passing on "reject" is the important part
+          retry(fn, retriesLeft - 1, interval).then(resolve, reject);
+        }, interval);
+      });
+  });
+}
