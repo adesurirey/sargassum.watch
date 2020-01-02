7996eda2b74cb9fbd1b71b1963f1639477818288
Arnaud de Surirey <adesurirey@gmail.com>
2020-01-02 19:59:19 +0100

    Migrate scrapper urls to environment variables

--- app/services/report_scrapper.rb
+++ app/services/report_scrapper.rb
@@ -1,41 +1,47 @@
 # frozen_string_literal: true
 
 class ReportScrapper
-  URLS = {
-    2019 => {
-      with:    "https://www.google.com/maps/d/kml?mid=1jQbixC2zZfrxgRTmZYULzDvHPwGmbsZh&forcekml=1",
-      without: "https://www.google.com/maps/d/kml?mid=1L0VAsV3pcHd5S4C0iBX6DtwCZdaMvENT&forcekml=1",
-    },
-    2018 => {
-      with:    "https://www.google.com/maps/d/kml?mid=1SI0WSsYwOA_MlPZS-mestIcqUTkhdbcd&forcekml=1",
-      without: "https://www.google.com/maps/d/kml?mid=1BBMjAfKlT11DYn6m2cPYOAezgt14Fb5G&forcekml=1",
-    },
-  }.freeze
-
   attr_reader :kml
 
-  def self.call(year:, kind:, attributes: {})
-    fail ArgumentError, "No url found for #{year} #{kind}" unless URLS[year][kind]
+  class << self
+    def url(year:, kind:)
+      ENV.fetch("REPORT_SCRAPPER_URL_#{year}_#{kind.upcase}")
+    end
 
-    new(year, kind, attributes).kml
+    def call(year:, kind:, attributes: {})
+      url = url(year: year, kind: kind)
+
+      new(url, year, attributes).kml
+    end
   end
 
   private
 
-  def initialize(year, kind, attributes)
+  def initialize(url, year, attributes)
+    @url = url
     @year = year
-    @url = URLS[year][kind]
-    @kml = Assets::KML.new(request.body, attributes)
-    clean_kml
+    @attributes = attributes
+    @kml = nil
+
+    scrap
   end
 
-  def clean_kml
-    @kml.placemarks.select! do |placemark|
-      placemark[:updated_at].year == @year
-    end
+  def scrap
+    @kml = to_kml(request)
+    clean_results!
   end
 
   def request
     Faraday.get @url
+  end
+
+  def to_kml(response)
+    Assets::KML.new(response.body, @attributes)
+  end
+
+  def clean_results!
+    @kml.placemarks.select! do |placemark|
+      placemark[:updated_at].year == @year
+    end
   end
 end
--- test/jobs/scrap_reports_job_test.rb
+++ test/jobs/scrap_reports_job_test.rb
@@ -89,7 +89,7 @@ class ScrapReportsJobTest < ActiveJob::TestCase
   private
 
   def stub_scrapper(kind)
-    url = ReportScrapper::URLS[Time.current.year][kind]
+    url = ReportScrapper.url(year: Time.current.year, kind: kind)
 
     stub_request(:get, url)
       .to_return(
