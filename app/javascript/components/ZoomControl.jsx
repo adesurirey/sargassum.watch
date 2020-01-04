42c4dff9a5f51315e6866eb22f09c009e9e30264
Arnaud de Surirey <adesurirey@gmail.com>
2020-01-04 21:23:28 +0100

    Migrate Mapbox controls style overrides to JSS

--- app/javascript/components/ZoomControl.jsx
+++ app/javascript/components/ZoomControl.jsx
@@ -5,6 +5,19 @@ import { makeStyles } from '@material-ui/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
 
+import MinusIconLight from '../images/minus-light.svg';
+import MinusIconDark from '../images/minus-dark.svg';
+import PlusIconLight from '../images/plus-light.svg';
+import PlusIconDark from '../images/plus-dark.svg';
+
+const buttonStyle = {
+  padding: 9,
+  backgroundSize: 'contain',
+  backgroundPosition: 'center',
+  backgroundOrigin: 'content-box',
+  backgroundRepeat: 'no-repeat',
+};
+
 const useStyles = makeStyles(theme => ({
   root: {
     position: 'absolute',
@@ -12,6 +25,32 @@ const useStyles = makeStyles(theme => ({
     right: 0,
     marginRight: theme.spacing(2),
     marginBottom: theme.spacing(2),
+
+    '& .mapboxgl-ctrl-group': {
+      backgroundColor: theme.palette.secondary.main,
+      boxShadow: [[theme.shadows[4]], '!important'],
+      borderRadius: theme.shape.borderRadius,
+      '& > button:hover': {
+        backgroundColor: theme.palette.secondary.dark,
+      },
+      '& > button + button': {
+        borderTop: `1px solid ${theme.palette.secondary.dark}`,
+      },
+      '& .mapboxgl-ctrl-zoom-in': {
+        ...buttonStyle,
+        backgroundImage:
+          theme.palette.type === 'light'
+            ? [[`url(${PlusIconLight})`], '!important']
+            : [[`url(${PlusIconDark})`], '!important'],
+      },
+      '& .mapboxgl-ctrl-zoom-out': {
+        ...buttonStyle,
+        backgroundImage:
+          theme.palette.type === 'light'
+            ? [[`url(${MinusIconLight})`], '!important']
+            : [[`url(${MinusIconDark})`], '!important'],
+      },
+    },
   },
 }));
 
