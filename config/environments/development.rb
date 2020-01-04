c90f58e684c7ac65a11666289df0861cb05a6ce1
Arnaud de Surirey <adesurirey@gmail.com>
2020-01-04 21:54:18 +0100

    Upgrade Rails 6.0.0 => 6.0.2.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store files locally.
+  # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
