02912804ca4bd7c12efc73d715c46ff5dda07c6e
Arnaud de Surirey <adesurirey@gmail.com>
2020-01-05 03:11:48 +0100

    Drop useless html body theme classname toggling

--- app/javascript/styles/withTheme.jsx
+++ app/javascript/styles/withTheme.jsx
@@ -2,43 +2,35 @@ import React, { useState, useEffect } from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import themeLight from './themeLight';
-import themeDark from './themeDark';
+import light from './themeLight';
+import dark from './themeDark';
 
-const themes = {
-  light: themeLight,
-  dark: themeDark,
-};
+const themes = { light, dark };
 
-const getMode = () => {
+const getColorScheme = () => {
   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     return 'dark';
   }
   return 'light';
 };
 
-const toggleBodyClass = mode => {
-  document.body.className = `theme-${mode}`;
-};
-
 const withTheme = Component => {
   const WithTheme = props => {
-    const [mode, setMode] = useState(getMode());
+    const [mode, setMode] = useState(getColorScheme());
     const theme = themes[mode];
-
-    const activateDarkMode = () => setMode('dark');
-    const activateLightMode = () => setMode('light');
 
     useEffect(() => {
       window
         .matchMedia('(prefers-color-scheme: dark)')
-        .addListener(e => e.matches && activateDarkMode());
+        .addListener(({ matches }) => {
+          matches && setMode('dark');
+        });
       window
         .matchMedia('(prefers-color-scheme: light)')
-        .addListener(e => e.matches && activateLightMode());
+        .addListener(({ matches }) => {
+          matches && setMode('light');
+        });
     }, []);
-
-    useEffect(() => toggleBodyClass(mode), [mode]);
 
     return (
       <MuiThemeProvider theme={theme}>
