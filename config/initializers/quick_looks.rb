c4ef327a456bb7c8669a536bd0b5889d5e0d8c49
Arnaud de Surirey <adesurirey@gmail.com>
2020-01-10 15:08:21 +0100

    Add popular search to geocoder

--- config/initializers/quick_looks.rb
+++ config/initializers/quick_looks.rb
@@ -1,34 +1,37 @@
 # frozen_string_literal: true
 
 QUICK_LOOKS = {
-  florida:    {
-    latitude:  27.28,
-    longitude: -81.32,
-    zoom:      6.02,
-  },
   guadeloupe: {
-    latitude:  16.22,
-    longitude: -61.49,
-    zoom:      8.41,
+    text:   "Guadeloupe",
+    center: [-61.49, 16.22],
+    zoom:   8.41,
   },
   martinique: {
-    latitude:  14.67,
-    longitude: -61,
-    zoom:      8.74,
+    text:   "Martinique",
+    center: [-61, 14.67],
+    zoom:   8.74,
   },
-  mexico:     {
-    latitude:  19.60,
-    longitude: -87.92,
-    zoom:      6.26,
+  quitana:     {
+    text:   "Quintana Roo",
+    place_name:   "Quintana Roo, Mexico",
+    center: [-87.92, 19.60],
+    zoom:   6.26,
   },
   tulum:      {
-    latitude:  20.22,
-    longitude: -87.43,
-    zoom:      8.62,
+    text:       "Tulum",
+    place_name: "Tulum, Quintana Roo, Mexico",
+    center:     [-87.43, 20.22],
+    zoom:       8.62,
   },
-  all:        {
-    latitude:  20.83,
-    longitude: -73.86,
-    zoom:      3.1,
+  florida:    {
+    text:       "Florida",
+    place_name: "Florida, United States of America",
+    center:     [-81.32, 27.28],
+    zoom:       6.02,
+  },
+  _all:       {
+    text:   "_all",
+    center: [-73.86, 20.83],
+    zoom:   3.1,
   },
 }.freeze
