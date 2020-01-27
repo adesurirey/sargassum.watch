08e1d995c7ce972e1f4744fe248c5e47bf7f378d
Arnaud de Surirey <adesurirey@gmail.com>
2020-01-27 12:45:28 +0100

    Update zoom control style

--- app/javascript/components/ZoomControl.jsx
+++ app/javascript/components/ZoomControl.jsx
@@ -12,6 +12,13 @@ import PlusIconDark from '../images/plus-dark.svg';
 
 const buttonStyle = {
   padding: 9,
+  '&:focus': {
+    boxShadow: 'none',
+    borderRadius: 'unset',
+  },
+};
+
+const backgroundStyle = {
   backgroundSize: 'contain',
   backgroundPosition: 'center',
   backgroundOrigin: 'content-box',
@@ -30,25 +37,37 @@ const useStyles = makeStyles(theme => ({
       backgroundColor: theme.palette.secondary.main,
       boxShadow: [[theme.shadows[4]], '!important'],
       borderRadius: theme.shape.borderRadius,
+      overflow: 'hidden',
+      '& > button': {
+        transition: theme.transitions.create('background-color', {
+          duration: theme.transitions.duration.shortest,
+        }),
+      },
       '& > button:hover': {
-        backgroundColor: theme.palette.secondary.dark,
+        backgroundColor: [[theme.palette.secondary.dark], '!important'],
       },
       '& > button + button': {
         borderTop: `1px solid ${theme.palette.secondary.dark}`,
       },
       '& .mapboxgl-ctrl-zoom-in': {
         ...buttonStyle,
-        backgroundImage:
-          theme.palette.type === 'light'
-            ? [[`url(${PlusIconLight})`], '!important']
-            : [[`url(${PlusIconDark})`], '!important'],
+        '& .mapboxgl-ctrl-icon': {
+          ...backgroundStyle,
+          backgroundImage:
+            theme.palette.type === 'light'
+              ? [[`url(${PlusIconLight})`], '!important']
+              : [[`url(${PlusIconDark})`], '!important'],
+        },
       },
       '& .mapboxgl-ctrl-zoom-out': {
         ...buttonStyle,
-        backgroundImage:
-          theme.palette.type === 'light'
-            ? [[`url(${MinusIconLight})`], '!important']
-            : [[`url(${MinusIconDark})`], '!important'],
+        '& .mapboxgl-ctrl-icon': {
+          ...backgroundStyle,
+          backgroundImage:
+            theme.palette.type === 'light'
+              ? [[`url(${MinusIconLight})`], '!important']
+              : [[`url(${MinusIconDark})`], '!important'],
+        },
       },
     },
   },
